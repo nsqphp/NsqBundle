1f2a80f820f27d47f71038450c7573487704371a
Konstantin Grachev <me@grachevko.ru>
2021-06-28 11:28:39 +0300

    Non retry messages must be acknowledged

---
+++ src/Messenger/AckUnrecoverableMessageListener.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Nsq\NsqBundle\Messenger;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
+use function Amp\Promise\wait;
+
+final class AckUnrecoverableMessageListener implements EventSubscriberInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            WorkerMessageFailedEvent::class => ['onMessageFailed', 500],
+        ];
+    }
+
+    public function onMessageFailed(WorkerMessageFailedEvent $event): void
+    {
+        if ($event->willRetry()) {
+            return;
+        }
+
+        $envelope = $event->getEnvelope();
+        $message = NsqReceivedStamp::getMessageFromEnvelope($envelope);
+
+        if ($message->isProcessed()) {
+            return;
+        }
+
+        wait($message->finish());
+    }
+}
